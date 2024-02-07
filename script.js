7526072c879a6856a242c471531f319eb4ccf170
marvin-2056 <157512215+marvin-2056@users.noreply.github.com>
2024-02-07 01:21:05 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,58 +1,47 @@
 $(document).ready(function(){
+
+  $("img").draggable(); 
+
+    containment: "#imageContainer"
+        scroll: false;
+
+  animateIMG(); 
+
   
+  function makeNewPosition(){
+      var h = $(window).height() - 50;
+      var w = $(window).width() - 50;
+      var nh = Math.floor(Math.random() * h);
+      var nw = Math.floor(Math.random() * w);
+      return [nh,nw];    
+  }
+
+  // Function to animate the image
+  function animateIMG(){
+      var newq = makeNewPosition();
+      $('img').animate({ top: newq[0], left: newq[1] }, 900, function(){
+          animateIMG(); // Recursive call to keep animating the image        
+      });
+  }
+
   
-  $("img").draggable( 
-  {
-      stop: function() {
-    
+  $("img").on('click', function(){
+      var text = "YAY you win i'd give you a cookie but I'm too lazy to add that in, anyway click the button to restart!"; // Text to display
+      var displayText = $("<p1>").text(text); 
+      $("body").append(displayText);
+      $('img').stop(); // Stop the animation
+  });
 
+  // Add a reset button to restart the animation
+  $("#resetButton").on('click', function(){
+      $("p1").remove();
+      animateIMG();
+      
 
-    let dogeLeft = $("img").position().left;
-    
-    if (dogeLeft> 300) {
-
-
-      console.log("we are past 300");
-      $('p').text("we are past 300");    
-    }
-    else{
-
-      console.log("we are not past 300");
-      $('p').text("we are not past 300");   
-
-    }
-    }
-  });  
+  });
 
 
 
-  $(document).ready(function(){
-    animateIMG();
+
+
 });
-
-function makeNewPosition(){
-
-
-    var h = $(window).height() - 50;
-    var w = $(window).width() - 50;
-
-    var nh = Math.floor(Math.random() * h);
-    var nw = Math.floor(Math.random() * w);
-
-    return [nh,nw];    
-
-}
-
-function animateIMG(){
-    var newq = makeNewPosition();
-    $('img').animate({ top: newq[0], left: newq[1] }, function(){
-      animateIMG();        
-    });
-
-};
-  
- 
- 
-  
-  
-})

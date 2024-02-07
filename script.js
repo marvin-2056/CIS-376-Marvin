75b54797ec924bad0dac2f529944f047533c02d8
marvin-2056 <157512215+marvin-2056@users.noreply.github.com>
2024-02-07 01:22:15 -0600

    script.js

--- script.js
+++ script.js
@@ -20,19 +20,19 @@ $(document).ready(function(){
   function animateIMG(){
       var newq = makeNewPosition();
       $('img').animate({ top: newq[0], left: newq[1] }, 900, function(){
-          animateIMG(); // Recursive call to keep animating the image        
+          animateIMG(); 
       });
   }
 
   
   $("img").on('click', function(){
-      var text = "YAY you win i'd give you a cookie but I'm too lazy to add that in, anyway click the button to restart!"; // Text to display
+      var text = "YAY you win i'd give you a cookie but I'm too lazy to add that in, anyway click the button to restart!"; 
       var displayText = $("<p1>").text(text); 
       $("body").append(displayText);
-      $('img').stop(); // Stop the animation
+      $('img').stop(); 
   });
 
-  // Add a reset button to restart the animation
+  
   $("#resetButton").on('click', function(){
       $("p1").remove();
       animateIMG();
